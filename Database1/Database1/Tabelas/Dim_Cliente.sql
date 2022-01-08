22c88eeccbf4592dae0e92d7cdd768ac6dfe9eff
paulo <paulo.sousa.ti@gmail.com>
2022-01-08 11:19:55 -0300

    primeiro commit

---
+++ Database1/Database1/Tabelas/Dim_Cliente.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Dim_Cliente]
+(
+	[Cod_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Cliente] NVARCHAR(200) NULL, 
+    [Cod_Cidade] NVARCHAR(200) NULL,
+    [Desc_Cidade] NVARCHAR(50) NULL, 
+    [Cod_Estado] NVARCHAR(200) NULL,
+    [Desc_Estado] NVARCHAR(50) NULL, 
+    [Cod_Regiao] NVARCHAR(200) NULL,
+    [Desc_Regiao] NVARCHAR(50) NULL,
+    [Cod_Segemento] NVARCHAR(200) NULL,
+    [Desc_Segemento] NVARCHAR(50) NULL 
+)
+
---
+++ Database1/Database1/Tabelas/Dim_Fabrica.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Dim_Fabrica]
+(
+	[Cod_Fabrica] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Fabrica] VARCHAR(200) NULL
+)
